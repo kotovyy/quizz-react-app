c84244af6e5f1f52ef12e9d24afe0f568bcd267d
Anastasiia Korbut <anast.korbut@gmail.com>
2023-01-12 13:05:10 +0300

    переписала на классы

--- src/App.js
+++ src/App.js
@@ -3,98 +3,129 @@ import Welcome from './components/Welcome'
 import Question from './components/Question'
 import {nanoid} from "nanoid"
 
-export default function App() {
+export default class App extends React.Component {
 
-  const [isQuizzStarted, setIsQuizzStarted] = React.useState(false);
-  const [isQuizzEnded, setIsQuizzEnded] = React.useState(false);
-  const [questions, setQuestions] = React.useState([]);
-  const [newGame, setNewGame] = React.useState(false);
-  const [count, setCount] = React.useState(0);
+  state = {
+        isQuizzStarted: false,
+        isQuizzEnded: false,
+        questions: [],
+        newGame: false,
+        count: 0
+    }
 
-  React.useEffect(() => {
+  getQuestions = () => {
     fetch("https://opentdb.com/api.php?amount=5")
         .then(res => res.json())
-        .then(data => setQuestions(data.results.map((item) => {
+        .then(data => this.setState(prevState => ({
+          questions: data.results.map((item) => {
             return {
               key: nanoid(),
               id: nanoid(),
-              question: convert(item.question),
-              correctAnswer: item.correct_answer,
+              question: this.convert(item.question),
+              correctAnswer: this.convert(item.correct_answer),
               incorrectAnswers: item.incorrect_answers,
               isCorrect: false,
               choosenAnswer: '',
               isCorrect: false
             }
+          })
         })))
-}, [newGame])
+}
 
-  function startQuizz() {
-    setIsQuizzStarted(true)
+  componentDidMount() {
+    this.getQuestions()
   }
 
-  function chooseOption(event, id) {
-    setQuestions(prevList => prevList.map(question => {
-      return question.id === id ? 
-          {
-            ...question, 
-            choosenAnswer: event.target.textContent,
-            isCorrect: event.target.textContent === question.correctAnswer ? true : false
-          } :
-          question
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.newGame !== this.state.newGame) {
+        this.getQuestions()
+    }
+  }
+
+  startQuizz = () => {
+    this.setState({
+      isQuizzStarted: true
+    })
+  }
+
+  chooseOption = (event, id) => {
+    this.setState(prevState => ({
+      questions: prevState.questions.map(question => {
+        return question.id === id ? 
+            {
+              ...question, 
+              choosenAnswer: event.target.textContent,
+              isCorrect: event.target.textContent === question.correctAnswer ? true : false
+            } :
+            question
+      })
     }))
   }
 
-  function checkAnswers() {
-    setIsQuizzEnded(true)
-    countCorrectAnswers()
+  checkAnswers = () => {
+    this.setState({
+      isQuizzEnded: true
+    })
+    this.countCorrectAnswers()
   }
 
-  function playAgain() {
-    setNewGame(prev => {return !prev})
-    setIsQuizzEnded(false)
-    setCount(0)
+  playAgain = () => {
+    this.setState({
+      isQuizzEnded: false,
+      count: 0
+    })
+
+    this.setState(prevState => ({
+      newGame: !prevState.newGame
+  }))
   }
 
-  function countCorrectAnswers() {
-    const count = questions.filter((item)=> item.isCorrect === true)
-    setCount(count.length)
+  countCorrectAnswers = () => {
+    const total = this.state.questions.filter((item)=> item.isCorrect === true)
+
+    this.setState({
+      count: total.length
+    })
   }
 
-  function convert(string) { 
+  convert = (string) => { 
     var text = document.createElement("textarea")
     text.innerHTML = string
     return text.value
-}
+  }
 
-const questionsList = questions.map((item) => {
-  return (
-      <Question
-        {...item}
-        chooseOption={chooseOption}
-        isQuizzEnded={isQuizzEnded}
-      />
-    )
-})
+  render() {
+
+    const questionsList = this.state.questions.map((item) => {
+      return (
+          <Question
+            {...item}
+            chooseOption={this.chooseOption}
+            isQuizzEnded={this.state.isQuizzEnded}
+          />
+        )
+    })
 
   return (
     <div className='wrapper'>
-       {isQuizzStarted ? 
+      {this.state.isQuizzStarted ? 
         <div className='question-wrapper'>
           <div className="questions">
             {questionsList}
           </div>
           <div className='footer'>
-            {isQuizzEnded && <p className='result'>You scored {count}/{questions.length} correct answers</p>}
-            <button className='btn btn--question' onClick={isQuizzEnded ? playAgain : checkAnswers}>
-              { isQuizzEnded ? 'Play again' : 'Check answers'}
+            {this.state.isQuizzEnded && <p className='result'>You scored {this.state.count}/{this.state.questions.length} correct answers</p>}
+            <button className='btn btn--question' onClick={this.state.isQuizzEnded ? this.playAgain : this.checkAnswers}>
+              { this.state.isQuizzEnded ? 'Play again' : 'Check answers'}
             </button>
           </div>
           
         </div>
-        : <Welcome startQuizz={startQuizz}/>
+        : <Welcome startQuizz={this.startQuizz}/>
         }
     </div>
-    
   );
+}
+
 }
 
